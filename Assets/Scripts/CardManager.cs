c40e54be7d2dacdf8141486b0b7410cf509f4298
jofa2442 <joey.f3@gmail.com>
2014-11-11 19:58:04 -0500

    Card Update
    Card prefab and Scripts to Mange/Change card

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardManager : MonoBehaviour {
+	public int cardNum;
+
+	public Texture[] fronts;
+
+	public Texture background;
+
+	public void showBack(){
+		renderer.enabled = true;
+		renderer.material.mainTexture = background;
+	}
+
+	public void showFront(){
+		renderer.enabled = true;
+		renderer.material.mainTexture = fronts[cardNum];
+	}
+
+	public void hideCard(){
+		renderer.enabled = false;
+	}
+	// Use this for initialization
+	void Start () {
+		showFront();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/CardTester.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardTester : MonoBehaviour {
+	public int state = 0;
+	CardManager cardMan;
+	// Use this for initialization
+	void Start () {
+		cardMan = GetComponent<CardManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUp(){
+		state++;
+		
+		switch(state){
+			case 1:
+				cardMan.showBack();
+				break;
+			case 2:
+				cardMan.showFront ();
+				state = 0;
+				break;
+			/*case 3:
+				cardMan.hideCard();
+				state = 0;
+				break;*/
+				
+		}
+	}
+}
