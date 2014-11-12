936423d7806ca440929e8fbe90a986d570cebcb8
jofa2442 <joey.f3@gmail.com>
2014-11-12 02:17:24 -0500

    More Card updates
    Switched to tags for face Up and Face down, hopefully this can be used
    to make the games update in real time

--- Assets/Scripts/CardTester.cs
+++ Assets/Scripts/CardTester.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class CardTester : Photon.MonoBehaviour {
 	public int state = 0;
-	CardManager cardMan;
-	
+	CardManager cardMan = new CardManager();
+	string tag;
+
 	// Use this for initialization
 	void Start () {
 		cardMan = GetComponent<CardManager>();
@@ -12,21 +13,38 @@ public class CardTester : Photon.MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		faceUpDown();
 	}	
-
+    
+	void OnPhotonSerialView(PhotonStream stream , PhotonMessageInfo info)
+	{
+		//NEED TO FIX THIS SO THEY UPDATE IN REAL TIME
+	}
+	
 	void OnMouseUp(){
 		state++;
-		
 		switch(state){
-			case 1:
-				cardMan.showBack();
-				break;
-			case 2:
-				cardMan.showFront();
-				state = 0;
-				break;
+		case 1:
+			tag = "Up";
+			break;
+		case 2:
+			tag = "Down";
+			state = 0;
+			break;
 		}
 	}
+	
+	void faceUpDown(){
+		if(tag == "Up"){
+			cardMan.showFront();
+			//Debug.Log("Up");
+		}
+		if(tag == "Down"){
+			cardMan.showBack();
+			//Debug.Log("Down");
+		}
+
+	}
+
 
 }
