a9354c1f2b0471abe15d4f54e2625b07b2ba5c8e
Yannis Papadopoulos <papadop.ya@gmail.com>
2014-11-22 23:27:06 -0500

    Card Prefabs
    I created a prefab fro every card except island, put new pictures for
    every card.

---
+++ Assets/Scripts/Land_Forest.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Land_Forest : MonoBehaviour {
+
+	public int cardNum;
+	public string cardName;	
+	public Texture front;
+	public Texture background;
+	public Texture tapped;
+	public Texture currentText;
+	
+	
+	public int state = 0;
+	string tag;
+	
+	
+	Vector3 axis = new Vector3(0f,1f,0f);
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		tag = "untap";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void showBack(){
+		renderer.enabled = true;
+		renderer.material.mainTexture = background;
+	}
+	
+	public void showFront(){
+		renderer.enabled = true;
+		renderer.material.mainTexture = front;
+	}
+	
+	public void hideCard(){
+		renderer.enabled = false;
+	}
+	
+	public void tapCard(){
+		renderer.enabled = true;
+		this.transform.RotateAround(transform.position,axis, 90);
+	}
+	
+	public void untapCard(){
+		renderer.enabled = true;
+		this.transform.RotateAround(transform.position,axis, -90);
+		
+	}
+	
+	public void setCardName(string name){
+		this.cardName=name;
+	}
+	
+	void OnMouseUp(){
+		
+		switch(state){
+		case 0:
+			tapCard ();
+			state = 1;
+			break;
+		case 1:	
+			untapCard ();
+			state = 0;
+			break;
+		}
+		
+	}
+	
+	void OnMouseOver(){
+		currentText = renderer.material.mainTexture;
+		Debug.Log ("I am selected");
+	}
+	
+	void OnMouseExit(){
+		currentText = null;
+	}
+}
